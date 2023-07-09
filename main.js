b5b9671eb72e1643fce0a44444167c117eb4feaf
iamyaxya <yaxyeciise88@gmail.com>
2023-07-09 09:26:58 +0300

    adding script file

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-let next = document.querySelector(".right");
-let previous = document.querySelector(".left");
-let image_1 = document.querySelector(".img-1");
-// image_1.style.marg = "100%"
-next.onclick = ()=>{
-    image_1.style.marginLeft = "-100%";
-}
+var menu = document.getElementById("menu");
+        var nav = document.querySelector(".nav");
+        var li = document.querySelectorAll(".nav ul li a");
+
+        nav.style.right = "-100%";
+
+        li.forEach((link)=>{
+            link.onclick = (e)=> {
+                nav.style.right = "-100%";
+                menu.src = "img/icons8-menu-30.png";
+            }
+        })
+
+        menu.onclick = function () {
+            if (nav.style.right == "-100%") {
+                nav.style.right = "0%";
+                menu.src = "img/icons8-cancel-64.png";
+            }
+            else {
+                nav.style.right = "-100%";
+                menu.src = "img/icons8-menu-30.png";
+            }
+        }
